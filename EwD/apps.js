d94709f0550c3f070258aae3796464c72f72fec8
TomaszMielecki <tomek.mielecki@gmail.com>
2023-01-08 19:57:37 +0100

    Oba wykresy działają

--- EwD/apps.js
+++ EwD/apps.js
@@ -1,19 +1,7 @@
-// "https://my.api.mockaroo.com/p_rojekt_.json/?key=22355650"
-//<td>${values.id}</td>
-//<td>${values.Model_buta}</td>
-//<td>${values.Nazwa_producenta}</td>
-//<td>${values.Rozmiar_buta}</td>
-//<td>${values.Płeć}</td>
-//<td>${values.Ocena}</td>
-//<td>${values.Cena}</td>
-//<td>${values.Limitowana_edycja}</td>
-//<td>${values.Ilość_w_magazynie}</td>
-//<td>${values.Ilość_sprzedanych}</td>
-//<td>${values.Data_dodania}</td>
-
 //fetch data
 
-async function apiData(){
+//----------------------------------------------------------------------------
+async function apiData1(){
     const apiLink = "https://my.api.mockaroo.com/p_rojekt_.json/?key=22355650";
 
     const response = await fetch(apiLink)
@@ -23,22 +11,31 @@ async function apiData(){
     const klientdata = datapoints.map((klient) => {
         console.log(klient)
         if (klient.id < 10) {
-            Opinia.push(klient.Ocena)
-            Płeć.push(klient.Płeć)
+          Opinia.push(klient.Ocena)
+         Płeć.push(klient.Płeć)
+       }
+   })
+}
+
+// --------------------------------------------------------------------------
+
+async function apiData2(){
+    const apiLink = "https://my.api.mockaroo.com/p_rojekt_.json/?key=22355650";
+
+    const response = await fetch(apiLink)
+    const datapoints = await response.json()
+    console.log(datapoints);
+
+    const sprzedazdata = datapoints.map((sprzedaz) => {
+        console.log(sprzedaz)
+        if (sprzedaz.id < 10) {
+            Sprzedane.push(sprzedaz.Ilość_sprzedanych)
+            Rozmiar.push(sprzedaz.Rozmiar_buta)
         }
     })
 }
 
+//----------------------------------------------------------------------------
 // let Opinia = [];
 //let Płeć = [];
 
-
-
-
-// const opinie = datapoints.map((amount) => amount.Ocena)
-   
-//Opinia = opinie
-
-//const plecklienta = datapoints.map((plec) => plec.Płeć)
-
-//Płeć = plecklienta
